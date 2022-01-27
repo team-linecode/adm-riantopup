b018948a5f26f3cab178e03ee743b132f34f77a2
team-linecode <linecode.team@gmail.com>
2022-01-27 11:32:18 +0700

    update

--- assets/plugins/datatable/datatable-2.js
+++ assets/plugins/datatable/datatable-2.js
@@ -2,14 +2,6 @@ $(function(e) {
 	$('#example').DataTable();
 
 			var table = $('#example1').DataTable();
-			$('button').click( function() {
-				var data = table.$('input, select').serialize();
-				alert(
-					"The following data would have been submitted to the server: \n\n"+
-					data.substr( 0, 120 )+'...'
-				);
-				return false;
-			});
 			$('#example2').DataTable( {
 				"scrollY":        "200px",
 				"scrollCollapse": true,
